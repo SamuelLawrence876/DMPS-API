ee5b519b84ac3e135dd9ffce2f66b4fdcc26b8ec
Samuel Lawrence <55727214+SamuelLawrence876@users.noreply.github.com>
2022-02-21 21:50:45 +0000

    eu

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.post("/create-payment-intent", async (req, res) => {
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
-    currency: "us",
+    currency: "eur",
     automatic_payment_methods: {
       enabled: true,
     },
