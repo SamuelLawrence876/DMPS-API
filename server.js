5c5eeb86f0d564001137c6dae4a6fb1531c277e8
Samuel Lawrence <55727214+SamuelLawrence876@users.noreply.github.com>
2022-02-21 00:53:09 +0000

    cc payments

--- server.js
+++ server.js
@@ -10,6 +10,37 @@ app.use(require("./routes/record"));
 const dbo = require("./db/conn");
 const Db = process.env.ATLAS_URI;
 const mongooose = require("mongoose");
+const bodyParser = require("body-parser");
+const stripe = require("stripe")(
+  "sk_test_51KUeHwIziYistER0nhmIolZovsBbsJeBxY4rocHCoD5h9uuDW8aretS3q7DWwjpwmOrZGswnWL0u4pSBR0obghZZ000jd7ryPC"
+);
+
+app.use(express.static("public"));
+app.use(express.json());
+
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 757.01;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "eur",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
 mongooose.connect(
   Db,
