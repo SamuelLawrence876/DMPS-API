c68ca242100ad415042ad10a3ec36f2b9a2e0fa5
Samuel Lawrence <55727214+SamuelLawrence876@users.noreply.github.com>
2022-05-04 23:43:05 +0000

    remove stripe

--- server.js
+++ server.js
@@ -8,33 +8,6 @@ app.use(express.json());
 // config
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-const stripe = require("stripe")(
-  "sk_test_51KnkBkHFncMRcd3qBvG8iSJonPD2LvdKhuWkAfqhN5rigEt0bdPnUnmTDOOgnNUeB2wxJ35DYOwsRCjaNwYcpV9S001OHpMcNV"
-);
-
-const calculateOrderAmount = (items) => {
-  // Replace this constant with a calculation of the order's amount
-  // Calculate the order total on the server to prevent
-  // people from directly manipulating the amount on the client
-  return 1400;
-};
-
-app.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body;
-
-  // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
-    currency: "eur",
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
-
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  });
-});
 
 // get driver connection
 const dbo = require("./db/conn");
