5d76f3795d27dff6d29d1aacb6c65dc707e3ea85
Samuel Lawrence <55727214+SamuelLawrence876@users.noreply.github.com>
2022-05-04 22:35:00 +0000

    server

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+// cors
 app.use(cors());
 app.use(express.json());
+
+// config
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+const stripe = require("stripe")(
+  "sk_test_51KnkBkHFncMRcd3qBvG8iSJonPD2LvdKhuWkAfqhN5rigEt0bdPnUnmTDOOgnNUeB2wxJ35DYOwsRCjaNwYcpV9S001OHpMcNV"
+);
+
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "eur",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
 // get driver connection
 const dbo = require("./db/conn");
@@ -13,16 +43,6 @@ const connectDB = require("./config/dbConn");
 connectDB();
 
 app.use(require("./routes/record"));
-// app.all("*", (req, res) => {
-//   res.status(404);
-//   if (req.accepts("html")) {
-//     res.sendFile(path.join(__dirname, "views", "404.html"));
-//   } else if (req.accepts("json")) {
-//     res.json({ error: "404 Not Found" });
-//   } else {
-//     res.type("txt").send("404 Not Found");
-//   }
-// });
 
 mongoose.connection.once("open", () => {
   console.log(`Successfully connected to Mongoose.`);
